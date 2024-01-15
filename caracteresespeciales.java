35351542e11949494c4f34bd7c3473fe2125cb2a
helensalas <202213863j@utea.edu.pe>
2024-01-15 04:26:26 -0500

    comit

---
+++ caracteresespeciales.java
@@ -0,0 +1,9 @@
+public class caracteresespeciales {
+    public static void main(String[] args){
+        System.out.println("una corchea: \u266A");
+        System.out.print("dos corcheas: \u266B");
+    }
+    }
+    
+    
+
